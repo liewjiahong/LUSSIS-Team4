6f2b209540de834aa9d1dc47f2eead5d241480f0
ashpri <ashpri@DESKTOP-EH74T64>
2017-07-18 14:38:25 +0800

    added new

--- LUSSIS-Team4/WebForm1.aspx.designer.cs
+++ LUSSIS-Team4/WebForm1.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace LUSSIS_Team4 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Button1 control.
+        /// CheckBox1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
     }
 }
